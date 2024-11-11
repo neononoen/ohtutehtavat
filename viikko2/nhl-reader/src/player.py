311d9b9f1d85b9b302307f3125ffb4e2178760c8
neononoen <roosa.nenonen@outlook.com>
2024-11-11 22:28:50 +0200

    Lisätty nhl-reader

---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.nationality = dict['nationality']
+        
+
+    def __str__(self):
+        return f"{self.name} {self.nationality}"
